79057adbb102c0d76e7bf9423b16fcd76dee4f97
quvinh <vinhhp2620@gmail.com>
2021-11-12 22:49:19 +0700

    fix migrate Order

--- database/migrations/2021_10_26_145041_create_orders_table.php
+++ database/migrations/2021_10_26_145041_create_orders_table.php
@@ -15,6 +15,7 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('user_id');
             $table->bigInteger('product_id');
             $table->integer('quantity');
             $table->timestamps();
