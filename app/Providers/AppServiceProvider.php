837174960b841f3586b0d576790d4125f743466e
quvinh <vinhhp2620@gmail.com>
2021-11-12 20:32:02 +0700

    app service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if (env('APP_ENV') == 'Production') {
+            URL::forceScheme('https');
+        }
     }
 
     /**
