0aa7f80a8412f492b631afe41c954c3db22fc443
quvinh <vinhhp2620@gmail.com>
2021-11-13 13:21:56 +0700

    edit fe admin

--- resources/views/web/product/edit.blade.php
+++ resources/views/web/product/edit.blade.php
@@ -5,22 +5,11 @@
 @endsection
 
 @section('content')
-    <form action="{{ route('update-product', $product->id) }}" method="post" enctype="multipart/form-data">
-        @csrf
-        @method('put')
-        <input type="text" name="category_id" value="{{ $product->category_id }}" required>
-        <input type="text" name="name" value="{{ $product->name }}" required>
-        <input type="text" name="price" value="{{ $product->price }}" required>
-        <input type="text" name="description" value="{{ $product->description }}" required>
-        <input type="text" name="quantity" value="{{ $product->quantity }}" required>
-        <input type="file" name="image" required>
-        <p>{{ $product->image }}</p>
-        <button type="submit">Update</button>
-    </form>
     <br><br>
     <div class="card">
         <div class="card-body">
             <h3 class="card-title">Thêm món</h3>
+            <br>
             <form action="{{ route('update-product', $product->id) }}" method="post" enctype="multipart/form-data">
                 @csrf
                 @method('put')
