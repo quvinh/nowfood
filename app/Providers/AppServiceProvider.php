e3008b404b318c43b8f9d65ff396a14d2a4b9ca5
quvinh <vinhhp2620@gmail.com>
2021-12-17 10:11:47 +0700

    edit env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,9 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        // if (env('APP_ENV') == 'Production') {
-        //     URL::forceScheme('https');
-        // }
+        if (env('APP_ENV') == 'Production') {
+            URL::forceScheme('https');
+        }
     }
 
     /**
