400006e8f672f7e8be3347878297965c933b9640
quvinh <vinhhp2620@gmail.com>
2021-12-20 16:12:28 +0700

    edit fe order

--- resources/views/web/order/get.blade.php
+++ resources/views/web/order/get.blade.php
@@ -13,10 +13,11 @@
             <tr>
                 <th scope="col">STT</th>
                 <th scope="col">ID Order</th>
-                <th scope="col">ID User</th>
+                <th scope="col">Username</th>
                 <th scope="col">Tên món</th>
                 <th scope="col">Đơn giá/món</th>
                 <th scope="col">Số lượng đặt</th>
+                <th scope="col">Ngày đặt</th>
                 <th scope="col">Thao tác</th>
             </tr>
         </thead>
@@ -28,10 +29,17 @@
                 <tr>
                     <th scope="row">{{ $i++ }}</th>
                     <td>{{ $item->id }}</td>
-                    <td>{{ $item->user_id }}</td>
+                    <!-- <td>{{ $item->user_id }}</td> -->
+                    <td>
+                        @php
+                            $getName = DB::table('users')->where('id', $item->user_id)->get();
+                        @endphp
+                        {{ $getName[0]->name }}
+                    </td>
                     <td>{{ $item->product->name }}</td>
                     <td>{{ $item->product->price }}</td>
                     <td>{{ $item->quantity }}</td>
+                    <td>{{ $item->created_at }}</td>
                     <td>
                         <a type="button" class="btn btn-danger" href="{{ route('delete-order',$item->id) }}">Xoá</a>
                     </td>
